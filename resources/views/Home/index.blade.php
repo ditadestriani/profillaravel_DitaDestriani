c2380a8e0530c4055bae15e1f97ef572bab3b394
ditadestriani <ditadestriani008@gmail.com>
2021-08-06 06:39:04 +0700

    first commit

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', 'DITA DESTRIANI')
+
+@section('content')
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <div class="card bg-light">
+        <div class="card-body">
+        <h1 style ="text-align:center;">PROFILE DITA DESTRIANI</h1>
+        </div>
+        <meta charset="UTF-8">
+        <img src="AA.jpeg" class="img-thumbnail"  width="1000" height="1200">
+        <title>Home<</title>
+        <style>
+        
+        body {
+            
+            background-color: #FFC0CB;
+            
+        }
+        </style>
+    </head>
+    <body>
+    </div>
+    </body>
+    </html>
+@endsection
